d1352dd094fd7d3bf07bbed8fd17ab438ce52d54
Gregori Orlov <Orlov.Gregori@yandex.ru>
2022-09-16 13:05:36 +0300

    скорпировать бек и фронт в одну директорию, проблема с CORS запросами

---
+++ frontend/src/mestoAuth.js
@@ -0,0 +1,60 @@
+export const BASE_URL = 'http://localhost:4000'
+
+export const register = (email, password) => {
+    return fetch(`${BASE_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ password: password, email: email })
+    })
+        .then((res) => {
+            try {
+                if (res.status === 201) {
+                    return res.JSON()
+                }
+            } catch (e) {
+                console.log(e);
+                return (e)
+            }
+        })
+        .then((res) => {
+            return res;
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
+export const authorize = (email, password) => {
+    return fetch(`${BASE_URL}/signin`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            password: password,
+            email: email
+        })
+    })
+        .then((response => response.json()))
+        .then((data) => {
+            if (data.token) {
+                localStorage.setItem('token', data.token);
+                return data;
+            }
+        })
+        .catch(err => console.log(err))
+}
+export const getContent = (token) => {
+    return fetch(`${BASE_URL}/users/me`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        }
+    })
+        .then(res => res.json())
+        .then((data) => {
+            return data;
+        })
+}
